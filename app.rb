efd221fc51209412fb0f699de67bba33a43621e8
Krystan Menne and Patrick Sullivan <patsul12@gmail.com>
2016-01-14 14:17:49 -0800

    add the New Phone Number path

--- app.rb
+++ app.rb
@@ -24,6 +24,31 @@ get('/contacts/new') do
 end
 
 get('/contacts/:id') do
-  @contact = Contact.find(params[:id].to_i())
+  @contact_id = params[:id].to_i()
+  @contact = Contact.find(@contact_id)
+  @phone_numbers = PhoneNumber.find_by_contact_id(@contact_id)
   erb(:contact)
+end
+
+post('/contacts/:id') do
+  @contact_id = params[:id].to_i()
+  @contact = Contact.find(@contact_id)
+  home_number = params[:home]
+  work_number = params[:work]
+  cell_number = params[:cell]
+
+  if home_number.length() > 0
+    home = PhoneNumber.new({:area_code    => params[:home_area],
+                            :phone_number => home_number,
+                            :type         => "home",
+                            :contact_id   => params[:id].to_i()})
+    home.save()
+  end
+  
+  redirect "/contacts/#{@contact_id}"
+end
+
+get('/contacts/:id/update') do
+  @contact = Contact.find(params[:id].to_i())
+  erb(:contact_update_form)
 end
--- spec/contact_list_integration_spec.rb
+++ spec/contact_list_integration_spec.rb
@@ -1,6 +1,7 @@
 require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
+include Capybara::DSL
 set(:show_exceptions, false)
 
 describe('the contact book page') do
@@ -13,24 +14,37 @@ describe('the contact book page') do
 
   describe('the add contact path', {:type => :feature}) do
     it('creates a new contact and displays it in a list.') do
-      visit('/')
-      click_link('New Contact')
-      fill_in_person_info()
-      click_button('Add')
+      create_a_contact()
       expect(page).to have_content('Barack')
     end
   end
 
   describe('the view contact path', {:type => :feature}) do
     it('displays information for a selected contact') do
-      visit('/')
-      click_link('New Contact')
-      fill_in_person_info()
-      click_button('Add')
+      create_a_contact()
       click_link('Obama, Barack')
       expect(page).to have_content('Barack')
     end
   end
+
+  describe('the new phone number path') do
+    it('creates a new phone number for a given contact') do
+      create_a_contact()
+      click_link('Obama, Barack')
+      click_link('Update Contact')
+      fill_in('home_area', :with => '555')
+      fill_in('home', :with => '8675309')
+      click_button('Update')
+      expect(page).to have_content('8675309')
+    end
+  end
+end
+
+def create_a_contact
+  visit('/')
+  click_link('New Contact')
+  fill_in_person_info()
+  click_button('Add')
 end
 
 def fill_in_person_info
