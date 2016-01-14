b5080daf5787f096e364b0dc50315361cfcf8905
Krystan Menne and Patrick Sullivan <patsul12@gmail.com>
2016-01-14 09:28:19 -0800

    add MailingAddress class

---
+++ lib/mailing_address.rb
@@ -0,0 +1,36 @@
+class MailingAddress
+  attr_reader(:house_number, :street, :city, :state, :zip, :type, :contact_id, :id)
+
+  @@mailing_addresses = []
+
+  def initialize(arguments)
+    @house_number = arguments[:house_number]
+    @street = arguments[:street]
+    @city = arguments[:city]
+    @state = arguments[:state]
+    @zip = arguments[:zip]
+    @type = arguments[:type]
+    @contact_id = arguments[:contact_id]
+    @id = @@mailing_addresses.length() + 1
+  end
+
+  def save
+    @@mailing_addresses << self
+  end
+
+  def self.all
+    @@mailing_addresses
+  end
+
+  def self.clear
+    @@mailing_addresses = []
+  end
+
+  def self.find (identification)
+    @@mailing_addresses.each() do |address|
+      if address.id() == identification
+        return address
+      end
+    end
+  end
+end
---
+++ spec/mailing_address_spec.rb
@@ -0,0 +1,77 @@
+require('rspec')
+require('mailing_address')
+require('pry')
+
+describe(MailingAddress) do
+  before() do
+    MailingAddress.clear()
+  end
+
+  describe('#initialize') do
+    it('adds house number, street, city, state, zip code, \
+        type, contact ID, and ID to an address') do
+      test_address = white_house()
+      expect(test_address.house_number()).to(eq('1600'))
+      expect(test_address.street()).to(eq('Pennsylvania Ave'))
+      expect(test_address.city()).to(eq('Washington'))
+      expect(test_address.state()).to(eq('DC'))
+      expect(test_address.zip()).to(eq('20500'))
+      expect(test_address.type()).to(eq('work'))
+      expect(test_address.contact_id()).to(eq(1))
+      expect(test_address.id()).to(eq(1))
+    end
+  end
+
+  describe('#save') do
+    it('adds an address to the array of saved addresss') do
+      test_address = white_house()
+      test_address.save()
+      expect(MailingAddress.all()).to(eq([test_address]))
+    end
+  end
+
+  describe('.all') do
+    it('is empty at first') do
+      expect(MailingAddress.all()).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it('empties out all of the saved addresss') do
+      white_house().save()
+      MailingAddress.clear()
+      expect(MailingAddress.all()).to(eq([]))
+    end
+  end
+
+  describe('find') do
+    it('returns an address by its id number') do
+      first_address = white_house()
+      first_address.save()
+      id_of_first_address = first_address.id()
+      second_address = north_pole()
+      second_address.save()
+      expect(MailingAddress.find(id_of_first_address)).to(eq(first_address))
+    end
+  end
+end
+
+def white_house
+  MailingAddress.new({:house_number => '1600',
+                      :street => 'Pennsylvania Ave',
+                      :city => 'Washington',
+                      :state => 'DC',
+                      :zip => '20500',
+                      :type => 'work',
+                      :contact_id => 1})
+end
+
+def north_pole
+  MailingAddress.new({:house_number => '101',
+                      :street => 'Santa Claus Lane',
+                      :city => 'North Pole',
+                      :state => 'AK',
+                      :zip => '99705',
+                      :type => 'home',
+                      :contact_id => 2})
+end
